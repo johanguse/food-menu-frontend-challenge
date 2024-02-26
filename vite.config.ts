618af408397438514137f3a7cf68df20196db213
Johan Guse <johanguse@gmail.com>
2024-02-26 10:28:49 -0300

    fix: gh action playwright

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
 
 const vitestConfig: VitestUserConfigInterface = {
   test: {
+    exclude: ['**/node_modules/**', './src/__tests__/e2e/**'],
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/__tests__/setup.ts',
