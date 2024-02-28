1581babea19925d8d7acf4150c1aa78bc5e31313
Johan Guse <johanguse@gmail.com>
2024-02-28 11:42:21 -0300

    fix: fix test text

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -5,6 +5,6 @@ import { describe, expect, it } from 'vitest';
 describe('Component | App', () => {
   it('should render the app component', async () => {
     render(<App />);
-    expect(screen.getByText('Ceviche Salmão')).toBeInTheDocument();
+    expect(screen.getByText('Ceviche de Salmão')).toBeInTheDocument();
   });
 });
